9b5667c2f0e64de8bc500ade2871e643f31e61c2
Harinath <hari@Haris-MacBook-Pro.local>
2020-05-22 05:36:20 +0530

    vsc commit

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -3,6 +3,7 @@ import { Cookie } from 'ng2-cookies';
 import { AppService } from '../app.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { SocketService } from '../socket.service';
 
 @Component({
   selector: 'app-friend',
@@ -12,15 +13,27 @@ import { ToastrService } from 'ngx-toastr';
 export class FriendsComponent implements OnInit {
 
   public fullName: any;
+  public authToken;
   constructor(
     public appService: AppService,
     public router: Router,
-    public toastr: ToastrService
+    public toastr: ToastrService,
+    public socketService: SocketService
   ) { }
 
   ngOnInit(): void {
     this.fullName = Cookie.get('fullName')
+    this.authToken = Cookie.get('authToken')
   }
+
+  public verifyUserConfirmation: any = () => {
+    this.socketService.verifyUser()
+      .subscribe(() => {
+        this.socketService.setUser(this.authToken);//in reply to verify user emitting set-user event with authToken as parameter.
+      }, (err) => {
+        this.toastr.error(err, "Some error occured");
+      });//end subscribe
+  }//end verifyUserConfirmation
 
   public logout = () => {
 
@@ -33,9 +46,9 @@ export class FriendsComponent implements OnInit {
           this.toastr.success(this.fullName.toUpperCase(), 'Logout Success! See You Again')
           this.router.navigate(['login']);
         } else {
-          this.toastr.error(apiResponse.message,"Error!")
+          this.toastr.error(apiResponse.message, "Error!")
         }
-      },(err) => {
+      }, (err) => {
         this.toastr.error("Some Error Occurred", "Error!");
       });
 
